dbe10cb93cbc8fb9bff381e9f2c115a048bd3e2e
toshuyu <toshuyu7@gmail.com>
2019-03-17 15:34:18 +0800

    - pkg ver to 1.0.1 - Add NUMBER_FORMAT_EXCEPTION with customized log

--- app/src/main/java/com/example/finalproject/crashstudy/ExceptionMaker.java
+++ app/src/main/java/com/example/finalproject/crashstudy/ExceptionMaker.java
@@ -2,15 +2,20 @@ package com.example.finalproject.crashstudy;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
+import com.crashlytics.android.Crashlytics;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ExceptionMaker {
+    private final static String TAG = ExceptionMaker.class.getSimpleName();
     public static final int RUNTIME_EXCEPTION = 0;
     public static final int CONCURRENTMODIFICATION_EXCEPTION = 1;
     public static final int BADTOKEN_EXCEPTION = 2;
+    public static final int NUMBER_FORMAT_EXCEPTION = 4;
 
     // TODO https://www.apteligent.com/technical-resource/top-5-crashes-on-android/
     // TODO https://stackoverflow.com/questions/33011319/what-are-the-list-of-android-specific-exceptions
@@ -26,13 +31,22 @@ public class ExceptionMaker {
             case BADTOKEN_EXCEPTION:
                 triggerBadTokenException(context);
                 break;
+            case NUMBER_FORMAT_EXCEPTION:
+                triggerNumberFormatException(context);
+                break;
         }
     }
 
     public static void triggerRuntimeException() {
         new Thread(new Runnable() {
             public void run() {
-                throw new RuntimeException();
+                try {
+                    throw new RuntimeException();
+                } catch (RuntimeException e){
+                    Crashlytics.setInt("classid", RUNTIME_EXCEPTION);
+                    Crashlytics.log(Log.ERROR, TAG, "this is a customized catch");
+                    Crashlytics.logException(e);
+                }
             }
         }).start();
     }
@@ -52,6 +66,11 @@ public class ExceptionMaker {
     public static void triggerBadTokenException(Context context) {
         // // https://stackoverflow.com/questions/18662239/android-view-windowmanagerbadtokenexception-unable-to-add-window-on-buider-s
         Intent intent = new Intent(context, BadTokenDemoActivity.class);
+        context.startActivity(intent);
+    }
+
+    public static void triggerNumberFormatException(Context context) {
+        Intent intent = new Intent(context, NumberFormatActivity.class);
         context.startActivity(intent);
     }
 }
